03bfd6d0b14bbd9295710686b4bc1ef5eae35538
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-02-11 22:59:04 -0300

    inital commit

---
+++ src/Post.js
@@ -0,0 +1,13 @@
+
+
+export function Post() {
+  return (
+    <>
+      <article>
+        <strong>Titulo da noticia</strong> <br />
+        <small>Subtítulo da notícia</small>
+      </article>
+      <br />
+    </>
+  );
+}
