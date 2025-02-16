473e2eddec36e04241993ab6d660172dbdc2af54
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-02-16 19:49:15 -0300

    feat: adjust styles  global

--- src/app/App.js
+++ src/app/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import GlobalStyle from '../styles/global';
 import Layout from '../Layout';
+import { ThemeProvider } from 'styled-components';
+import themes from '../styles/themes';
+
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={themes.light}>
       <GlobalStyle />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/PostList/Post/index.js
+++ src/components/PostList/Post/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Container } from './styles';
 
 export default function Post({ title, description }) {
+
   return (
     <Container>
       <h2>{title}</h2>
