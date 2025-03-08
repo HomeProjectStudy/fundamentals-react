ef317fb1b69d62ec596ba6d71d97af622afa3d08
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-03-08 19:09:16 -0300

    feat: saved theme on storage

--- src/contexts/ThemeContext-class.js
+++ src/contexts/ThemeContext-class.js
@@ -1,15 +1,19 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 
 export const ThemeContextClass = createContext({});
 
 
 export function ThemeProviderClass({children}) {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || 'dark');
 
   function handleToogleTheme(){
     setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'));
   }
+
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
 
   return (
     <ThemeContextClass.Provider value={{
