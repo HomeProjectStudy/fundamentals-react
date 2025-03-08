3dd4d60cdc0205b6090c56b4c7b3d271fdd5e437
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-03-08 14:10:25 -0300

    refactor: understanding props, this, and functions in react class components

--- src/app/App.js
+++ src/app/App.js
@@ -6,33 +6,62 @@ import { ThemeProvider } from 'styled-components';
 import themes from '../styles/themes';
 
 
-function App() {
-  const [theme, setTheme] = useState('dark');
+// function App() {
+//   const [theme, setTheme] = useState('dark');
 
 
 
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark;
-  }, [theme]);
+//   const currentTheme = useMemo(() => {
+//     return themes[theme] || themes.dark;
+//   }, [theme]);
 
-  function handleToogleTheme(){
-    setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'));
+// function handleToogleTheme(){
+//   setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'));
+// }
+
+
+//   useEffect(() => {
+//     localStorage.setItem('theme', JSON.stringify(theme));
+//   }, [theme]);
+
+//   return (
+//     <ThemeProvider theme={currentTheme}>
+//       <GlobalStyle />
+//       <Layout
+//         onToggleTheme={handleToogleTheme}
+//         selectedTheme={theme}
+//       />
+//     </ThemeProvider>
+//   );
+// }
+class App extends React.Component {
+
+  state = {
+    theme:'dark'
+  };
+
+
+
+  handleToogleTheme = () => {
+    this.setState((prevState) => ({
+      theme: prevState.theme === 'dark' ? 'light' : 'dark'
+    }));
+  };
+
+  render() {
+    const { theme } = this.state;
+
+    return (
+      <ThemeProvider theme={themes[theme] || themes.dark}>
+        <GlobalStyle />
+        <Layout
+          onToggleTheme={this.handleToogleTheme}
+          selectedTheme={theme}
+        />
+      </ThemeProvider>
+    );
   }
-
-
-  useEffect(() => {
-    localStorage.setItem('theme', JSON.stringify(theme));
-  }, [theme]);
-
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      <Layout
-        onToggleTheme={handleToogleTheme}
-        selectedTheme={theme}
-      />
-    </ThemeProvider>
-  );
 }
+
 
 export default App;
